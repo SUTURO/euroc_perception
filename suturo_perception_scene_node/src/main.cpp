bbc8c4302baa234430e8577a3c5628253c88979f
ofenrohr <ofenrohr@tzi.de>
2014-08-01 15:57:20 +0200

    start migration of segmentation pipeline

---
+++ suturo_perception_scene_node/src/main.cpp
@@ -0,0 +1,56 @@
+#include "ros/ros.h"
+#include <ros/package.h>
+#include "suturo_scene_node.h"
+
+
+int main (int argc, char** argv)
+{
+  ros::init(argc, argv, "suturo_scene_node");
+  ros::NodeHandle nh;
+
+	std::string imageTopic = "/suturo/euroc_scene_image";
+  std::string cloudTopic = "/suturo/euroc_scene_cloud";
+  ROS_INFO("Image topic is: %s", imageTopic.c_str());
+  ROS_INFO("Cloud topic is: %s", cloudTopic.c_str());
+
+  SuturoSceneNode node(nh, imageTopic, cloudTopic);
+
+  ROS_INFO("                    _____ ");
+  ROS_INFO("                   |     | ");
+  ROS_INFO("                   | | | | ");
+  ROS_INFO("                   |_____| ");
+  ROS_INFO("             ____ ___|_|___ ____ ");
+  ROS_INFO("            ()___)         ()___) ");
+  ROS_INFO("            // /|           |\\ \\\\ ");
+  ROS_INFO("           // / |           | \\ \\\\ ");
+  ROS_INFO("          (___) |___________| (___) ");
+  ROS_INFO("          (___)   (_______)   (___) ");
+  ROS_INFO("          (___)     (___)     (___) ");
+  ROS_INFO("          (___)      |_|      (___) ");
+  ROS_INFO("          (___)  ___/___\\___   | | ");
+  ROS_INFO("           | |  |           |  | | ");
+  ROS_INFO("           | |  |___________| /___\\ ");
+  ROS_INFO("          /___\\  |||     ||| //   \\\\ ");
+  ROS_INFO("         //   \\\\ |||     ||| \\\\   // ");
+  ROS_INFO("         \\\\   // |||     |||  \\\\ // ");
+  ROS_INFO("          \\\\ // ()__)   (__() ");
+  ROS_INFO("                ///       \\\\\\ ");
+  ROS_INFO("               ///         \\\\\\ ");
+  ROS_INFO("             _///___     ___\\\\\\_ ");
+  ROS_INFO("            |_______|   |_______| ");
+
+  ROS_INFO("   ____  __  __ ______  __  __   ___   ____                            ");
+  ROS_INFO("  / __/ / / / //_  __/ / / / /  / _ \\ / __ \\                           ");
+  ROS_INFO(" _\\ \\  / /_/ /  / /   / /_/ /  / , _// /_/ /                           ");
+  ROS_INFO("/___/_ \\____/_ /_/__  \\____/  /_/|_| \\____/______   ____  ____    _  __");
+  ROS_INFO("  / _ \\  / __/  / _ \\ / ___/  / __/  / _ \\/_  __/  /  _/ / __ \\  / |/ /");
+  ROS_INFO(" / ___/ / _/   / , _// /__   / _/   / ___/ / /    _/ /  / /_/ / /    / ");
+  ROS_INFO("/_/    /___/  /_/|_| \\___/  /___/  /_/    /_/    /___/  \\____/ /_/|_/  ");
+  ROS_INFO(" ~ suturo_scene_node");
+                                                                       
+  // ROS_INFO("           suturo_perception READY");
+  ros::MultiThreadedSpinner spinner(2);
+  spinner.spin();
+  return (0);
+}
+// vim: tabstop=2 expandtab shiftwidth=2 softtabstop=2: 
---
+++ suturo_perception_scene_node/src/suturo_scene_node.h
@@ -0,0 +1,47 @@
+#ifndef SUTURO_PERCEPTION_SCENE_NODE_H
+#define SUTURO_PERCEPTION_SCENE_NODE_H
+
+#include "ros/ros.h"
+
+#include "perception_utils/logger.h"
+#include "suturo_perception_msgs/GetScene.h"
+
+#include <pcl_ros/point_cloud.h>
+#include <sensor_msgs/PointCloud2.h>
+
+class SuturoSceneNode
+{
+  public:
+    SuturoSceneNode(ros::NodeHandle &nodeHandle, std::string imageTopic, std::string depthTopic);
+    bool getScene(suturo_perception_msgs::GetScene::Request &req, suturo_perception_msgs::GetScene::Response &res);
+
+    void receive_cloud(const sensor_msgs::PointCloud2ConstPtr& inputCloud);
+    void clusterFromProjection(pcl::PointCloud<pcl::PointXYZRGB>::Ptr object_clusters, pcl::PointCloud<pcl::PointXYZRGB>::Ptr original_cloud, std::vector<int> *removed_indices_filtered, std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> &extracted_objects, std::vector<pcl::PointIndices::Ptr> &original_indices);
+  private:
+    ros::NodeHandle nodeHandle_;
+    ros::ServiceServer clusterService_;
+		std::string imageTopic_;
+    std::string cloudTopic_;
+		perception_utils::Logger logger;
+		int idx_;
+		bool processing_;
+		
+		pcl::ModelCoefficients::Ptr coefficients_; 
+    
+    // Set default parameters
+    float zAxisFilterMin;
+    float zAxisFilterMax;
+    float downsampleLeafSize;
+    int planeMaxIterations;
+    double planeDistanceThreshold;
+    double ecClusterTolerance; 
+    int ecMinClusterSize;
+    int ecMaxClusterSize;  
+    double prismZMin;
+    double prismZMax;
+    double ecObjClusterTolerance;
+    int ecObjMinClusterSize;
+    int ecObjMaxClusterSize;
+};
+
+#endif // TABLE_FROM_DEPTH_IMAGE_NODE_H
