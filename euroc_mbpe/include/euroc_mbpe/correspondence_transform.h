bf651f02e5a66f0582c58d442e434d244fbf6b50
Patrick Mania <pmania@euroc02.(none)>
2014-08-08 12:51:24 +0200

    the correspondence transform code has been fully moved to a seperate library

--- euroc_mbpe/include/euroc_mbpe/correspondence_transform.h
+++ euroc_mbpe/include/euroc_mbpe/correspondence_transform.h
@@ -18,22 +18,59 @@
 
 class CorrespondenceTransform
 {
+  private:
+    pcl::PointCloud<pcl::PointXYZ>::Ptr model_correspondences_;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr observed_correspondences_;
+    // Observed points in Vector format
+    Eigen::VectorXf y0_;
+
+    // Initial pose guess
+    Eigen::Matrix< float, 6, 1 > initial_x_;
+    // The (iteratively) estimated pose
+    Eigen::Matrix< float, 6, 1 > x_;
+
+    int max_iterations_;
+    int iterations_;
+    // The pose estimation is done, when 
+    // abs( (deltax.norm() / x.norm()) < convergence_threshold_
+    // E.g. the change in the pose parameters is small enough ...
+    double convergence_threshold_;
+
   public:
 
-  CorrespondenceTransform()
-  {
-    // Empty constructor
-  }
-  Eigen::VectorXf getVectorFromPointCloud4(pcl::PointCloud<pcl::PointXYZ>::Ptr p);
+    CorrespondenceTransform()
+    {
+      // Assume a neutral pose first ...
+      initial_x_[0] = initial_x_[1] = initial_x_[2] = initial_x_[3] = initial_x_[4] = initial_x_[5] = 0;
+      x_ = initial_x_;
+      max_iterations_ = 20;
+      iterations_ = 0;
+      convergence_threshold_ = 1e-6;
+    }
+    Eigen::VectorXf getVectorFromPointCloud4(pcl::PointCloud<pcl::PointXYZ>::Ptr p);
 
-  Eigen::Matrix4f getRotationMatrixFromPose(Eigen::Matrix< float, 6, 1 > pose);
+    Eigen::Matrix4f getRotationMatrixFromPose(Eigen::Matrix< float, 6, 1 > pose);
 
-  // Transform a cloud with a given pose + a small varation of one of the parameters in the pose
-  // variation_idx indicates which index should be added by e
-  pcl::PointCloud<pcl::PointXYZ>::Ptr transformVariedPose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
-      Eigen::Matrix< float, 6, 1 > pose, int variation_idx, float e);
+    // Transform a cloud with a given pose + a small varation of one of the parameters in the pose
+    // variation_idx indicates which index should be added by e
+    pcl::PointCloud<pcl::PointXYZ>::Ptr transformVariedPose(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
+        Eigen::Matrix< float, 6, 1 > pose, int variation_idx, float e);
 
-  pcl::PointCloud<pcl::PointXYZ>::Ptr generateBoxModelFromPose(Eigen::Matrix< float, 6, 1 > pose);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr generateBoxModelFromPose(Eigen::Matrix< float, 6, 1 > pose);
+
+    // It's necessary to call this function.
+    // This method takes two Pointclouds that describe corresponding points.
+    // The format is as follows:
+    //   - The first point in model_correspondences corresponds to
+    //     the first point in observed_correspondences
+    //   - The rest is analog ...
+    //   - You should have atleast 3 corresponding points for a 6DOF pose estimation
+    void setCorrespondences(pcl::PointCloud<pcl::PointXYZ>::Ptr model_correspondences, pcl::PointCloud<pcl::PointXYZ>::Ptr observed_correspondences);
+
+    void execute();
+    Eigen::Matrix< float, 6, 1 > getEstimatedPose();
+    // How many iteration have been done for the pose estimation?
+    int getIterationCount();
 };
 
 #endif
--- euroc_mbpe/src/correspondence_transform.cpp
+++ euroc_mbpe/src/correspondence_transform.cpp
@@ -97,11 +97,83 @@ pcl::PointCloud<pcl::PointXYZ>::Ptr CorrespondenceTransform::generateBoxModelFro
         c.x = x;
         c.y = y;
         c.z = z; 
-        std::cout << c << std::endl;
+        // std::cout << c << std::endl;
         result->push_back(c);
       }
     }
   }
   pcl::transformPointCloud(*result, *result, getRotationMatrixFromPose(pose) );
   return result;
+}
+
+void CorrespondenceTransform::setCorrespondences(pcl::PointCloud<pcl::PointXYZ>::Ptr model_correspondences, pcl::PointCloud<pcl::PointXYZ>::Ptr observed_correspondences)
+{
+  model_correspondences_ = model_correspondences;
+  observed_correspondences_ = observed_correspondences;
+  y0_ = getVectorFromPointCloud4(observed_correspondences);
+}
+
+void CorrespondenceTransform::execute()
+{
+
+  // TODO Exception when model correspondences and observed_correspondences differ
+  // TODO Exception when correspondence size < 3
+ 
+  // Observed points in Vector format
+  // Eigen::VectorXf y0;
+  // y0 = ct.getVectorFromPointCloud4(observed_correspondences);
+
+  // Define a small variation that will be used to calculate the
+  // effect of varyiing the different parameters of the pose
+  double e = 0.0001;
+  // Eigen::Matrix< float, 12, 6 > jacobian = Eigen::Matrix< float, 12, 6 >::Zero();
+  Eigen::MatrixXf jacobian(12,6); // correspondence_points * 3 x 6 pose parameters
+  // int iterations;
+  for(iterations_=0; iterations_<max_iterations_; iterations_++)
+  {
+    // Where will the points of the model be with the given pose?
+    // This will be the basis for our error calculation
+    pcl::PointCloud<pcl::PointXYZ>::Ptr predicted_model_pts (new pcl::PointCloud<pcl::PointXYZ>);
+    // Calculate the model points w.r.t the current pose estimation
+    pcl::transformPointCloud(*model_correspondences_, *predicted_model_pts, getRotationMatrixFromPose(x_) );
+    Eigen::VectorXf y = getVectorFromPointCloud4(predicted_model_pts);
+
+
+    // Transform the points with a slight change of the pose parameters
+    pcl::PointCloud<pcl::PointXYZ>::Ptr changed_pointcloud (new pcl::PointCloud<pcl::PointXYZ>);
+    for(int idx=0;idx<6;idx++)
+    {
+      changed_pointcloud = transformVariedPose(model_correspondences_, x_, idx, e);
+      Eigen::VectorXf pts_after_variation = getVectorFromPointCloud4(changed_pointcloud);
+      // ... and write these points in the jacobian column per column
+      for(int j=0;j<12;j++)
+      {
+        jacobian(j,idx) = (pts_after_variation[j] - y[j]) / e;
+      }
+    }
+    // Get the delta between the observed points
+    // and the predicted points w.r.t to the current pose
+    Eigen::VectorXf deltay = y0_ - y;
+    std::cout << "Residual: " << deltay.norm() << std::endl;
+    Eigen::Matrix< float, 6, 1 > deltax;
+    Eigen::MatrixXf pinv = (jacobian.transpose() * jacobian).inverse() * jacobian.transpose();
+    deltax = pinv * deltay;
+
+    if( abs( (deltax.norm() / x_.norm()) < 1e-6 ) )
+    {
+      // change in transformation is nearly zero .... 
+      break;
+    }
+    // Update pose estimate
+    x_ = x_ + deltax;
+  }
+}
+Eigen::Matrix< float, 6, 1 > CorrespondenceTransform::getEstimatedPose()
+{
+  return x_;
+}
+
+int CorrespondenceTransform::getIterationCount()
+{
+  return iterations_;
 }
