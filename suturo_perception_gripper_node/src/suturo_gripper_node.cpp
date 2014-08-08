54981eb9a3397f7b3f70d769fda48d5270f58df3
ofenrohr <ofenrohr@tzi.de>
2014-08-08 15:47:29 +0200

    gripper node up and running

---
+++ suturo_perception_gripper_node/src/suturo_gripper_node.cpp
@@ -0,0 +1,103 @@
+#include "suturo_gripper_node.h"
+
+#include <perception_utils/point_cloud_operations.h>
+#include <perception_utils/publisher_helper.h>
+#include <suturo_perception_segmentation/projection_segmenter.h>
+#include <suturo_perception_pipeline/pipeline.h>
+
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+using namespace suturo_perception;
+
+SuturoGripperNode::SuturoGripperNode(ros::NodeHandle &n, std::string imageTopic, std::string depthTopic) : 
+  nodeHandle_(n), 
+  imageTopic_(imageTopic),
+  cloudTopic_(depthTopic)
+{
+	logger = Logger("SuturoPerceptionGripperNode");
+  clusterService_ = nodeHandle_.advertiseService("/suturo/GetGripper", 
+    &SuturoGripperNode::getGripper, this);
+	idx_ = 0;
+
+  markerPublisher_ = nodeHandle_.advertise<visualization_msgs::Marker>("/suturo/cuboid_markers_gripper", 0);
+  maxMarkerId_ = 0;
+
+}
+
+bool
+SuturoGripperNode::getGripper(suturo_perception_msgs::GetGripper::Request &req, suturo_perception_msgs::GetGripper::Response &res)
+{
+	res.id = idx_;
+	idx_++;
+
+  pipelineData_ = PipelineData::Ptr(new PipelineData());
+	
+	ros::Subscriber sub = nodeHandle_.subscribe<sensor_msgs::PointCloud2>(cloudTopic_, 1, boost::bind(&SuturoGripperNode::receive_cloud,this, _1));
+	
+	logger.logInfo("Waiting for processed cloud");
+  ros::Rate r(20); // 20 hz
+  // cancel service call, if no cloud is received after 10s
+  boost::posix_time::ptime cancelTime = boost::posix_time::second_clock::local_time() + boost::posix_time::seconds(10);
+	processing_ = true;
+  while(processing_)
+  {
+    if(boost::posix_time::second_clock::local_time() >= cancelTime)
+    {
+      processing_ = false;
+			logger.logError("No sensor data available. Aborting.");
+			return false;
+		}
+    ros::spinOnce();
+    r.sleep();
+  }
+  logger.logInfo("done with segmentation, starting pipeline");
+    
+  /****************************************************************************/
+  Pipeline::execute(pipelineData_, pipelineObjects_);
+  /****************************************************************************/
+
+  logger.logInfo("done with perception pipeline, sending result");
+  for (int i = 0; i < pipelineObjects_.size(); i++)
+  {
+    logger.logInfo("sending object");
+    if (pipelineObjects_.at(i) == NULL)
+    {
+      logger.logError("pipeline object is NULL! investigate this!");
+      continue;
+    }
+    res.objects.push_back(pipelineObjects_[i]->toEurocObject());
+  }
+
+  logger.logInfo("results sent, publishing markers");
+  PublisherHelper::publish_marker(pipelineObjects_, "/sdepth", markerPublisher_, &maxMarkerId_);
+
+  return true;
+}
+
+
+void
+SuturoGripperNode::receive_cloud(const sensor_msgs::PointCloud2ConstPtr& inputCloud)
+{
+	logger.logInfo("image and cloud incoming...");
+  if (!processing_)
+  {
+    logger.logInfo("processing_ == false, aborting...");
+    return;
+  }
+  boost::posix_time::ptime s = boost::posix_time::microsec_clock::local_time();
+	
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in (new pcl::PointCloud<pcl::PointXYZRGB>());
+	pcl::fromROSMsg(*inputCloud,*cloud_in);
+  
+  
+  ProjectionSegmenter projection_segmenter;
+  if (!projection_segmenter.segment(cloud_in, pipelineData_, pipelineObjects_))
+  {
+    return;
+  }
+
+	processing_ = false;
+}
---
+++ suturo_perception_gripper_node/src/suturo_gripper_node.h
@@ -0,0 +1,43 @@
+#ifndef SUTURO_PERCEPTION_SCENE_NODE_H
+#define SUTURO_PERCEPTION_SCENE_NODE_H
+
+#include "ros/ros.h"
+
+#include "perception_utils/logger.h"
+#include "perception_utils/pipeline_object.hpp"
+#include "perception_utils/pipeline_data.hpp"
+#include "suturo_perception_msgs/GetGripper.h"
+
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_cloud.h>
+#include <sensor_msgs/PointCloud2.h>
+
+class SuturoGripperNode
+{
+  public:
+    SuturoGripperNode(ros::NodeHandle &nodeHandle, std::string imageTopic, std::string depthTopic);
+    bool getGripper(suturo_perception_msgs::GetGripper::Request &req, suturo_perception_msgs::GetGripper::Response &res);
+
+    void receive_cloud(const sensor_msgs::PointCloud2ConstPtr& inputCloud);
+    void clusterFromProjection(pcl::PointCloud<pcl::PointXYZRGB>::Ptr object_clusters, pcl::PointCloud<pcl::PointXYZRGB>::Ptr original_cloud, std::vector<int> *removed_indices_filtered, std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> &extracted_objects, std::vector<pcl::PointIndices::Ptr> &original_indices);
+  private:
+    ros::NodeHandle nodeHandle_;
+    ros::ServiceServer clusterService_;
+		std::string imageTopic_;
+    std::string cloudTopic_;
+    suturo_perception::Logger logger;
+		int idx_;
+		bool processing_;
+
+    ros::Publisher markerPublisher_;
+    int maxMarkerId_;
+    void publish_marker(suturo_perception::PipelineObject::VecPtr &objects);
+		
+		suturo_perception::PipelineObject::VecPtr pipelineObjects_;
+    suturo_perception::PipelineData::Ptr pipelineData_;
+		// pcl::ModelCoefficients::Ptr coefficients_; 
+    
+    
+};
+
+#endif // TABLE_FROM_DEPTH_IMAGE_NODE_H
