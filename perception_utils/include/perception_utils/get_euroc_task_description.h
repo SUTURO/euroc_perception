8462696998f080e8a60557c058ebff6ea2a28d7f
ofenrohr <ofenrohr@tzi.de>
2014-08-21 15:54:09 +0200

    get results of yaml pars0r

---
+++ perception_utils/include/perception_utils/get_euroc_task_description.h
@@ -0,0 +1,28 @@
+#ifndef GET_EUROC_TASK_DESCRIPTION_H
+#define GET_EUROC_TASK_DESCRIPTION_H
+
+#include <ros/ros.h>
+#include <suturo_msgs/Task.h>
+#include <perception_utils/logger.h>
+
+namespace suturo_perception
+{
+  class EurocTaskClient 
+  {
+  public:
+    EurocTaskClient(ros::NodeHandle &n);
+    suturo_msgs::Task getTaskDescription();
+    bool requestTaskDescription();
+    void receiveTaskDescription(const boost::shared_ptr<const suturo_msgs::Task> task);
+    
+  protected:
+    static const std::string YAML_PARS0R_TOPIC;
+    
+    Logger logger;
+    ros::NodeHandle nodeHandle_;
+    suturo_msgs::Task taskDescription_;
+    bool waitingForDescription_;
+  };
+}
+
+#endif
