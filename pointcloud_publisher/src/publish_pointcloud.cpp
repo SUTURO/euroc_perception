b08699f7f58ce4349cb9c26753847f433f0d5f43
ofenrohr <ofenrohr@tzi.de>
2014-09-11 18:56:44 +0200

    PERE-30 cleanup

--- pointcloud_publisher/src/publish_pointcloud.cpp
+++ pointcloud_publisher/src/publish_pointcloud.cpp
@@ -12,7 +12,6 @@
 #include <sensor_msgs/image_encodings.h>
 
 #include <cv_bridge/cv_bridge.h>
-#include <opencv/highgui.h>
 #include "opencv2/imgproc/imgproc.hpp"
 
 #include <message_filters/subscriber.h>
@@ -25,6 +24,8 @@
 #include <boost/program_options.hpp>
 
 #include <perception_utils/logger.h>
+
+#include "projector.h"
 
 namespace enc = sensor_msgs::image_encodings;
 namespace po = boost::program_options;
@@ -41,7 +42,6 @@ int cloud_idx = 0;
 std::string depth_topic = "";
 std::string rgb_topic = "";
 std::string frame = "";
-std::string frame_depth = "";
 std::string frame_rgb = "";
 std::string output_topic = "";
 bool verbose = false;
@@ -50,188 +50,32 @@ void printTransform(const tf::StampedTransform &transform)
 { 
   tf::Vector3 ot = transform.getOrigin();
   tf::Quaternion qt = transform.getRotation();
-  //ROS_INFO("  translation: [ %f , %f , %f , %f, %f, %f ]", ot[0], ot[1], ot[2], tf::getYaw(qt), tf::getPitch(qt), tf::getRoll(qt));
   ROS_INFO("  translation: [ %f , %f , %f ]", ot[0], ot[1], ot[2]);
   ROS_INFO("  rotation:    [ %f , %f , %f , %f ]", qt.x(), qt.y(), qt.z(), qt.w());
-}
-
-// Thanks for Jan for the code
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr depth_project(const cv::Mat &depth_image_in,
-const cv::Mat &rgb_image, const tf::StampedTransform &transform)
-{
-  printTransform(transform);
-  cv::Mat depth_image;
-  if (depth_image_in.type() == CV_16U)
-   depth_image_in.convertTo(depth_image, CV_32F, 0.001, 0.0);
-  else
-   depth_image = depth_image_in;
-
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-  // TODO cloud->header.stamp = time;
-  cloud->height = depth_image.rows;
-  cloud->width = depth_image.cols;
-  // cloud->is_dense = false;
-  cloud->is_dense = true; // The PC should be dense, since you use the image coords to index the points
-  cloud->points.resize(cloud->height * cloud->width);
-  register const float
-     constant = 1.0f / (0.8203125 * cloud->width),
-     bad_point = std::numeric_limits<float>::quiet_NaN();
-  register const int
-     centerX = (cloud->width >> 1),
-     centerY = (cloud->height >> 1);
-
-  // DO THE M(A|E)TH
-  double fov_v = 0.817109355f;
-  double fov_h = 1.047f;
-  double h1 = tan(fov_h/2);
-  double v1 = tan(fov_v/2);
-
-  int seq = 0;
-  tf::TransformListener listener;
-
-#pragma omp parallel for
-  for (int y = 0; y < depth_image.rows; ++y)
-  {
-   pcl::PointXYZRGB *pPt = &cloud->points[y * depth_image.cols];
-
-   const float *pDepth = depth_image.ptr<float>(y);
-   const cv::Vec3b *pBGR = rgb_image.ptr<cv::Vec3b>(y);
-
-   for (register int u = 0; u < centerX*2; ++u, ++pPt, ++pDepth, ++pBGR)
-   {
-     /*
-     pPt->r = pBGR->val[2];
-     pPt->g = pBGR->val[1];
-     pPt->b = pBGR->val[0];
-     */
-
-     float depth = *pDepth;
-     // Check for invalid measurements
-     if (isnan(depth) || depth == 0)
-     {
-       // not valid
-       pPt->x = pPt->y = pPt->z = bad_point;
-       continue;
-     }
-     /*
-     pPt->x = depth;
-     pPt->y = depth * (h1 - (2*h1 *( u/(double)640) ));
-     pPt->z = depth * (v1 - (2*v1 *( y/(double)480) ));
-     */
-     
-     pPt->z = depth;
-     pPt->y = -depth * (v1 - (2*v1 *( y/(double)480) ));
-     pPt->x = -depth * (h1 - (2*h1 *( u/(double)640) ));
-
-     geometry_msgs::PointStamped p1;
-     p1.header.seq = seq++;
-     p1.header.stamp = ros::Time(0);
-     p1.header.frame_id = frame;
-     p1.point.x = pPt->x;
-     p1.point.y = pPt->y;
-     p1.point.z = pPt->z;
-     
-     //tf::Vector3 p3;
-     //p3.m_floats[0] = pPt->x;
-     //p3.m_floats[1] = pPt->y;
-     //p3.m_floats[2] = pPt->z;
-     
-     //tf::Vector3 p4;
-
-     geometry_msgs::PointStamped p2;
-     
-     
-     geometry_msgs::PointStamped p3;
-
-     
-     int tries = 0;
-     bool done = false;
-     while (tries < 5 && !done)
-     {
-      try{
-        //listener.waitForTransform(frame, frame_rgb, ros::Time(0), ros::Duration(3.0));
-        //listener.transformPoint(frame_rgb, p1, p3);
-        
-        //tf::StampedTransform t;
-        tf::Stamped<tf::Point> tp1;
-        tf::Stamped<tf::Point> tp2;
-        //listener.waitForTransform(frame, frame_rgb, ros::Time(0), ros::Duration(3.0));
-        tf::pointStampedMsgToTF(p1, tp1);
-        //listener.lookupTransform(frame_rgb, tp1.frame_id_, tp1.stamp_, t);
-        tp2.setData(transform * tp1);
-        pointStampedTFToMsg(tp2, p2);
-        
-       // if (p2.point.x != p3.point.x || p2.point.y != p3.point.y || p2.point.z != p3.point.z)
-       // {
-       //   ROS_INFO("(%f,%f,%f) - (%f,%f,%f)", p2.point.x, p2.point.y, p2.point.z, p3.point.x, p3.point.y, p3.point.z);
-       // }
-        //p4 = transform * p3;
-        //ROS_INFO("transform point success: (%f, %f, %f)", p2.point.x, p2.point.y, p2.point.z);
-        done = true;
-      } catch (...) {
-      } 
-      tries++;
-     }
-     if (!done)
-     {
-      //ROS_ERROR("transform point failed!");
-      //ros::Duration(1.0).sleep();
-      pPt->r = 255;
-      pPt->g = 0;
-      pPt->b = 255;
-      continue;
-     }
-    
-     //printTransform(t);
-     
-     int pixx = (( 640.0 * ( p2.point.x * h1 - p2.point.y ) ) / ( 2.0 * p2.point.x * h1 ));
-     int pixy = ( 480.0 * ( p2.point.x * v1 - p2.point.z ) ) / ( 2.0 * p2.point.x * v1 );
-     //int pixx = (( 640.0 * ( p4.m_floats[0] * h1 - p4.m_floats[1] ) ) / ( 2.0 * p4.m_floats[0] * h1 ));
-     //int pixy = ( 480.0 * ( p4.m_floats[0] * v1 - p4.m_floats[2] ) ) / ( 2.0 * p4.m_floats[0] * v1 );
-     if (pixx < 0 || pixx > 640 || pixy < 0 || pixy > 480)
-     {
-       //ROS_INFO("pixx = %d, pixy = %d, (%f,%f,%f) - (%f,%f,%f)", pixx, pixy, p2.point.x, p2.point.y, p2.point.z, p3.point.x, p3.point.y, p3.point.z);
-      pPt->r = 255;
-      pPt->g = 0;
-      pPt->b = 255;
-     }
-     else
-     {
-      cv::Vec3b bgrPixel = rgb_image.at<cv::Vec3b>(pixy, pixx);
-      pPt->r = bgrPixel.val[2];
-      pPt->g = bgrPixel.val[1];
-      pPt->b = bgrPixel.val[0];
-     }
-   }
-  }
-  return cloud;
 }
 
 bool getTransform(const ros::NodeHandle &node, const ros::Time &t, tf::StampedTransform &transform_) 
 {
   tf::TransformListener listener;
-
   int tries = 0;
   ros::Rate rate(10.0);
-  while (node.ok() && tries < 10){
-    try{
+  while (node.ok() && tries < 10)
+  {
+    try
+    {
       listener.waitForTransform(frame_rgb, frame, ros::Time(0), ros::Duration(3.0));
-      listener.lookupTransform(frame_rgb, frame,
-                               ros::Time(0), transform_);
-      ROS_INFO("lookupTransform success!");
+      listener.lookupTransform(frame_rgb, frame, ros::Time(0), transform_);
       return true;
     }
-    catch (tf::TransformException &ex) {
+    catch (tf::TransformException &ex) 
+    {
       ROS_ERROR("%s",ex.what());
       ros::Duration(1.0).sleep();
     }
-
     rate.sleep();
     tries++;
   }
-
   return false;
-
 }
 
 
@@ -267,18 +111,21 @@ void receive_depth_and_rgb_image(
 
   tf::StampedTransform transform;
   getTransform(nodeHandle, depthImage->header.stamp, transform);
-  printTransform(transform);
+  if (verbose)
+    printTransform(transform);
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_out = 
-    depth_project(resized_depth, resized_img, transform);
+    CloudProjector::depth_project(resized_depth, resized_img, transform);
 
 	// write pcd
   // pcl::PCDWriter writer;
+    /*
   std::stringstream ss;
   ss << "euroc_cloud_" << cloud_idx << ".pcd";
 	cloud_idx++;
   // writer.write(ss.str(), *cloud_out);
   if(verbose)
     std::cerr << "Saved " << cloud_out->points.size () << " data points" << std::endl;
+  */
 	
   sensor_msgs::PointCloud2 pub_message;
   pcl::toROSMsg(*cloud_out, pub_message );
@@ -287,7 +134,9 @@ void receive_depth_and_rgb_image(
   pub_cloud.publish(pub_message);
   
   boost::posix_time::ptime e = boost::posix_time::microsec_clock::local_time();
-  logger.logTime(s, e, "generate pointcloud");
+  std::stringstream ss;
+  ss << "generate pointcloud on " << output_topic;
+  logger.logTime(s, e, ss.str());
 }
 
 int main (int argc, char** argv)
