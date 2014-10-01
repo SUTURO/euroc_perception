f4b4ab124bb1464439dd5783d7fe3db0889a87a1
ofenrohr <ofenrohr@tzi.de>
2014-10-01 15:11:03 +0200

    scene classifier that publishes result labels

---
+++ suturo_perception_classifier/scripts/client_scene.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+from __builtin__ import object
+
+__author__ = 'smurpheus'
+
+import rospy
+
+from suturo_perception_msgs.srv import Classifier
+from suturo_perception_msgs.srv import GetScene
+
+
+
+if __name__ == "__main__":
+    s = 'height,centroid,color'
+    rospy.wait_for_service('suturo/GetScene')
+    try:
+        perceived_objects = rospy.ServiceProxy('suturo/GetScene', GetScene)
+        object =  perceived_objects(s).objects
+        print object
+        rospy.wait_for_service('suturo/SceneClassifier')
+        try:
+            classified = rospy.ServiceProxy('suturo/SceneClassifier', Classifier)
+            for each in object:
+                classified_object =  classified(each).classifiedObject
+                print classified_object
+        except rospy.ServiceException, e:
+            print "Service call failed: %s" % e
+    except rospy.ServiceException, e:
+        print "Service call failed: %s" % e
+
