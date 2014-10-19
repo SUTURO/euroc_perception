bd71e1af71d09f91870bf92fd72f12f9af1fba0c
mhertzer <mhertzer@tzi.de>
2014-10-19 15:54:50 +0000

    #PERE-31 #PERE-43 finished rebuild of classifier. Now doesn't crash at t1_v2 fixed height bug of blue handle, more stable recognition

--- suturo_perception_classifier/src/suturo_perception_classifier/classifier.py
+++ suturo_perception_classifier/src/suturo_perception_classifier/classifier.py
@@ -3,24 +3,30 @@ __author__ = 'smurpheus'
 import random
 import copy
 import numpy as np
-
 import rospy
 import cv2
 from os import system
+from Parser import Parser
 from suturo_msgs.msg import Task
 from suturo_perception_msgs.srv import ClassifierResponse
 from sklearn import tree
+from sklearn.naive_bayes import GaussianNB
 
 
-euroc_suturo_shape_mapping = {'3': '2', '1': '1'}
+euroc_suturo_shape_mapping = {'3': '2', '1': '1', '2': '3'}
 
 
 class Classifier(object):
+    selectable_attributes = ['color_class', 'height', 'volume', 'cuboid', 'color_hsv', 'color_rgb']
+    color_classes = ['0000ff', '00ff00', '00ffff', 'ff0000', 'ff00ff', 'ffff00']
+    color_class_number_mapping = {'0000ff':0, '00ff00':1, '00ffff':2, 'ff0000':3, 'ff00ff':4, 'ffff00':5}
+    selected_attributes = ['color_class', 'height', 'volume']
     original_objects = None
     cubeized_objects = {}
     size_tresh_perc = 0.01
     color_tresh = 10
-    number_of_data = 10
+    number_of_data = 1000
+    classifier_list = {}
 
     def __init__(self, task, logging):
         self.logging = logging
@@ -28,24 +34,24 @@ class Classifier(object):
         rospy.Subscriber("/suturo/yaml_pars0r", Task, self.set_yaml_infos)
         self.clf = tree.DecisionTreeClassifier()
 
-
     def set_yaml_infos(self, data):
         objects = data.objects
         self.original_objects = objects
         all_data = {}
         if self.logging >= 1: print(">>>> Receiving Objects from YAML")
         if self.logging >= 2: print("Objects Received from YAML: \r\n %s"%objects)
-        for object in objects:
-            name = object.name
-            color = object.color
-            primitive = object.primitives
-            self.cubeized_objects[name] = {'color': color, 'dimensions': self.get_surrounding_cuboid(object)}
-            randomized_objects = self.randomize_objects(self.cubeized_objects, self.number_of_data,
+        pars = Parser()
+        parsed = pars.convert_yaml_objects(objects,self.selected_attributes)
+
+
+        randomized_objects = self.randomize_objects(parsed, self.number_of_data,
                                                         self.size_tresh_perc,
                                                         self.color_tresh)
-            all_data[name] = randomized_objects
-        data, labels = self.convert_to_dataset(all_data)
-        rnd_data, rnd_labels = [],[]#self.create_random_obstacles(number=1000)@TODO relative to task
+        data, labels = pars.convert_to_dataset(randomized_objects)
+        #print self.create_random_obstacles(number=1000, attributes=self.selected_attributes)
+        rnd_data, rnd_labels = self.create_random_obstacles(number=1000, attributes=self.selected_attributes)#@TODO relative to task
+        for each in self.original_objects:
+            self.classifier_list[each.name] = self.create_classifier_for_object(each, data+rnd_data, labels+rnd_labels)
         if self.logging >= 3: print("All Data + Labels \r\n %s  \r\n %s"
                                     %( (labels + rnd_labels), (data+rnd_data)))
         self.clf.fit(data+rnd_data, labels+rnd_labels)
@@ -54,125 +60,92 @@ class Classifier(object):
         # system('dot -Tpng tree.dot -o tree.png')
         # system('feh tree.png &')
 
-    def get_surrounding_cuboid(self, object):
-        if len(object.primitives) == 1:
-            primitive = object.primitives[0]
-            print(primitive.dimensions)
-            dimensions = list(primitive.dimensions)
-            dimensions.sort()
-            if primitive.type == 3:
-                z = dimensions[0]
-                x = 2 * dimensions[1]
-                y = x
-                return [x, y, z]
-            if primitive.type == 1:
-                return [dimensions[0], dimensions[1], dimensions[2]]
-            return [0, 0, 0]  #
-        if len(object.primitives) > 1:
-            z, y, x = 0, 0, 0
-            for primitive in object.primitives:
-                dimensions = list(primitive.dimensions)
-                dimensions.sort()
-                if primitive.type == 3:
-                    z += dimensions[0]
-                    if x < 2 * dimensions[1]: x = 2 * dimensions[1]
-                    if y < 2 * dimensions[1]: y = 2 * dimensions[1]
-                if primitive.type == 1:
-                    z += dimensions[2]
-                    if x < 2 * dimensions[0]: x = dimensions[0]
-                    if y < 2 * dimensions[1]: y = dimensions[1]
-            return [x, y, z]
+
+    def create_classifier_for_object(self, object, data, labels):
+        new_labels = []
+        for each in labels:
+            if each != object.name:
+                new_labels.append('unknown')
+            else:
+                new_labels.append(each)
+        cl = tree.DecisionTreeClassifier()
+        cl = GaussianNB()
+        cl.fit(data, new_labels)
+        return cl
+
 
     def randomize_objects(self, objects, number, size_treshold, color_treshold):
+        randomized = []
         for object in objects:
-            randomized = []
             for i in range(0, number):
-                r = int(objects[object]['color'][:2], 16)
-                g = int(objects[object]['color'][2:4], 16)
-                b = int(objects[object]['color'][-2:], 16)
-                r_rand = random.randrange(color_treshold)
-                g_rand = random.randrange(color_treshold)
-                b_rand = random.randrange(color_treshold)
-                if r + r_rand > 255:
-                    r -= r_rand
-                else:
-                    r += r_rand
-                if g + g_rand > 255:
-                    g -= g_rand
-                else:
-                    g += g_rand
-                if b + b_rand > 255:
-                    b -= b_rand
-                else:
-                    b += b_rand
-                if len(hex(r)[2:]) < 2:
-                    r_hex = '0' + hex(r)[2:]
-                else:
-                    r_hex = hex(r)[2:]
-                if len(hex(g)[2:]) < 2:
-                    g_hex = '0' + hex(g)[2:]
-                else:
-                    g_hex = hex(g)[2:]
-                if len(hex(b)[2:]) < 2:
-                    b_hex = '0' + hex(b)[2:]
-                else:
-                    b_hex = hex(b)[2:]
+                r_obj = copy.copy(object)
+                if object.has_key('cuboid'):
+                    x = object['cuboid'][0]
+                    y = object['cuboid'][1]
+                    z = object['cuboid'][2]
+                    # decimize
+                    xd = random.uniform(0.0,size_treshold)
+                    yd = random.uniform(0.0,size_treshold)
+                    zd = random.uniform(0.0,size_treshold)
+                    x_rand = x * xd
+                    y_rand = y * yd
+                    z_rand = z * zd
+                    plus_or_min = random.randrange(2)
+                    if plus_or_min > 0:
+                        new_x = x + x_rand
+                    else:
+                        new_x = x - x_rand
+                    if plus_or_min > 0:
+                        new_y = y + y_rand
+                    else:
+                        new_y = y - y_rand
+                    if plus_or_min > 0:
+                        new_z = z + z_rand
+                    else:
+                        new_z = z - z_rand
 
-                x = objects[object]['dimensions'][0]
-                y = objects[object]['dimensions'][1]
-                z = objects[object]['dimensions'][2]
-                # decimize
-                xd = x * (10 ** len(str(x))) * size_treshold
-                yd = y * (10 ** len(str(y))) * size_treshold
-                zd = z * (10 ** len(str(z))) * size_treshold
-                x_rand = random.randrange(int(xd)) / (10. ** len(str(x)))
-                y_rand = random.randrange(int(yd)) / (10. ** len(str(y)))
-                z_rand = random.randrange(int(zd)) / (10. ** len(str(z)))
-                plus_or_min = random.randrange(2)
-                if plus_or_min > 0:
-                    new_x = x + x_rand
-                else:
-                    new_x = x - x_rand
-                if plus_or_min > 0:
-                    new_y = y + y_rand
-                else:
-                    new_y = y - y_rand
-                if plus_or_min > 0:
-                    new_z = z + z_rand
-                else:
-                    new_z = z - z_rand
-                rand_obj = copy.copy(objects[object])
-                rand_obj['color'] = r_hex + g_hex + b_hex
-                rand_obj['dimensions'][0] = new_x
-                rand_obj['dimensions'][1] = new_y
-                rand_obj['dimensions'][2] = new_z
-                randomized.append(rand_obj)
+                if object.has_key('height'):
+                    old_height = object['height']
+                    height__tresh = old_height * size_treshold
+                    r_obj['height'] = random.uniform(old_height-height__tresh, old_height+height__tresh)
+                if object.has_key('volume'):
+                    old_vol = object['volume']
+                    vol_tresh = old_vol * size_treshold
+                    r_obj['volume'] = random.uniform(old_vol*vol_tresh, old_vol+vol_tresh)
+                if object.has_key('cuboid'):
+                    r_obj['cuboid']= [new_x, new_y, new_z]
+
+                if object.has_key('color_hsv') or object.has_key('color_rgb'):
+                    r = object['color_rgb'][0]
+                    g = object['color_rgb'][1]
+                    b = object['color_rgb'][2]
+                    r_rand = random.randrange(color_treshold)
+                    g_rand = random.randrange(color_treshold)
+                    b_rand = random.randrange(color_treshold)
+                    if r + r_rand > 255:
+                        r -= r_rand
+                    else:
+                        r += r_rand
+                    if g + g_rand > 255:
+                        g -= g_rand
+                    else:
+                        g += g_rand
+                    if b + b_rand > 255:
+                        b -= b_rand
+                    else:
+                        b += b_rand
+                if object.has_key('color_hsv'):
+                    hsv_color = cv2.cvtColor(np.uint8([[[b, g, r]]]), cv2.COLOR_BGR2HSV)
+                    r_obj['color_hsv'] = [hsv_color[0][0][0], hsv_color[0][0][1], hsv_color[0][0][2]]
+                if object.has_key('color_rgb'):
+                    r_obj['color_rgb'] = [r,g,b]
+                randomized.append(r_obj)
         return randomized
 
-    def convert_euroc_object(self, eu_object):
-        color = eu_object['color']
-        r = int(color[:2], 16)
-        g = int(color[2:4], 16)
-        b = int(color[-2:], 16)
-        hsv_color = cv2.cvtColor(np.uint8([[[b, g, r]]]), cv2.COLOR_BGR2HSV)
-        x = eu_object['dimensions'][0]
-        y = eu_object['dimensions'][1]
-        z = eu_object['dimensions'][2]
-        return [[hsv_color[0][0][0], hsv_color[0][0][1], hsv_color[0][0][2], x]]#,
-                # [hsv_color[0][0][0], hsv_color[0][0][1], hsv_color[0][0][2], y],
-                # [hsv_color[0][0][0], hsv_color[0][0][1], hsv_color[0][0][2], z]]
 
-    def convert_to_dataset(self, raw_data):
-        data = []
-        labels = []
-        for key in raw_data:
-            for object in raw_data[key]:
-                data += self.convert_euroc_object(object)
-            labels += [key] * len(raw_data[key])
-        return data, labels
+
 
     def classify_object(self, object):
-        class_dict = {'red_cube': 1, 'green_cylinder': 2, 'blue_handle': 0, 'obstacle': 0}
         # load object
         unclassified_object = object.unclassifiedObject
         if self.logging >= 2 : print("Got Object to Classify: \r\n %s" %unclassified_object)
@@ -185,48 +158,75 @@ class Classifier(object):
         s = int(s * 255)
         v = int(v * 255)
         hsv_color = np.uint8([[[h, s, v]]])
-        # bgr_color = cv2.cvtColor(hsv_color, cv2.COLOR_HSV2BGR)
-        # r = bgr_color[0][0][2]
-        # g = bgr_color[0][0][1]
-        # b = bgr_color[0][0][0]
+        bgr_color = cv2.cvtColor(hsv_color, cv2.COLOR_HSV2BGR)
+        r = bgr_color[0][0][2]
+        g = bgr_color[0][0][1]
+        b = bgr_color[0][0][0]
+        color_class = self.color_class_number_mapping[unclassified_object.c_color_class]
+        volume = unclassified_object.c_volume
         #sort edges
         if unclassified_object.c_cuboid_success:
             edges = list(unclassified_object.object.primitives[0].dimensions)
-            edges.sort()
         #build classifyable object and classify it
-        classifyable_unclassified_object = [h, s, v, height]# + edges
+        classifyable_unclassified_object = []
+        if 'color_class' in self.selected_attributes:
+            classifyable_unclassified_object.append(color_class)
+        if 'height' in self.selected_attributes:
+            classifyable_unclassified_object.append(height)
+        if 'volume' in self.selected_attributes:
+            classifyable_unclassified_object.append(volume)
+        if 'cuboid' in self.selected_attributes:
+            classifyable_unclassified_object += unclassified_object.object.primitives[0].dimensions
+        if 'color_hsv' in self.selected_attributes:
+            classifyable_unclassified_object += [h,s,v]
+        if 'color_rgb' in self.selected_attributes:
+            classifyable_unclassified_object += [r,g,b]
+
+
         if self.logging >= 1: print("Object to Classify: \r\n %s"%classifyable_unclassified_object)
-        class_name = self.clf.predict(classifyable_unclassified_object)
+        class_name = 'unknown'
+        for each in self.classifier_list:
+            classn = self.classifier_list[each].predict(classifyable_unclassified_object)[0]
+            if classn != 'unknown':
+                class_name = classn
+
+
         # print class_name
-        # class_name = self.lolloosed(r,g,b)
-        unclassified_object.c_shape = class_dict[class_name[0]]
+        # unclassified_object.c_shape = self.get_shape_of_class(class_name[0])
         unclassified_object.object.id = class_name[0]
         resp = ClassifierResponse()
         resp.classifiedObject = unclassified_object
         if self.logging >= 1: print("Classified Object as: %s"%class_name)
         return resp
 
-    def create_random_obstacle(self, max_height=2):
-        height = random.randrange(0, int(max_height) * 1000) / 1000.
-        h = random.randrange(0, 180)
-        s = random.randrange(230, 255)
-        v = random.randrange(230, 255)
-        return [h, s, v, height]
 
-    def create_random_obstacles(self, max_height=2, number=100):
+
+
+    def create_random_obstacle(self, max_height=2, attributes=[]):
+        rnd_obj = []
+        if 'color_class' in attributes:
+            rnd_obj.append(self.color_class_number_mapping[self.color_classes[random.randrange(len(self.color_classes))]])
+        if 'height' in attributes:
+            rnd_obj.append(random.randrange(0, int(max_height) * 1000) / 1000.)
+        if 'volume' in attributes:
+            rnd_obj.append(random.uniform(0.01, 0.5))#TODO BAAAD hard coded.... change it!
+        if 'cuboid' in attributes:
+            pass #todo dunno how to rand this
+        if 'color_hsv' in attributes:
+            h = random.randrange(0, 180)
+            s = random.randrange(230, 255)
+            v = random.randrange(230, 255)
+            rnd_obj.append([h,s,v])
+        if 'color_rgb' in attributes:
+            r = random.randrange(0, 255)
+            g = random.randrange(0, 255)
+            b = random.randrange(0, 255)#todo colors needs to be depended....
+            rnd_obj.append([r,g,b])
+        return rnd_obj
+
+    def create_random_obstacles(self, max_height=2, number=100, attributes=[]):
         obstacles = []
         for i in range(0,number):
-            obstacles.append(self.create_random_obstacle(max_height=max_height))
+            obstacles.append(self.create_random_obstacle(max_height=max_height, attributes=attributes))
         labels = ['obstacle'] * number
         return obstacles, labels
-
-    def lolloosed(self, r, g, b):
-        colors = [r, g, b]
-        ind = colors.index(max(colors))
-        if ind == 0:
-            return ['red_cube']
-        if ind == 1:
-            return ['green_cylinder']
-        if ind == 2:
-            return ['blue_handle']
-        return ['unkown']
