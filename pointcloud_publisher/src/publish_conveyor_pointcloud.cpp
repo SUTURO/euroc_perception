41e1b66feb3de3c8cd713b754f0207c4a3eddecb
ofenrohr <ofenrohr@tzi.de>
2014-10-20 22:02:18 +0200

    PERE-37 generate conveyor pointcloud for task6_v1

--- pointcloud_publisher/src/publish_conveyor_pointcloud.cpp
+++ pointcloud_publisher/src/publish_conveyor_pointcloud.cpp
@@ -39,7 +39,7 @@ ros::Publisher pub_cloud;
 Logger logger("publish_conveyor_pointcloud");
 
 
-int cloud_point_cnt = 3000;
+int cloud_point_cnt = 6000;
 
 // Parameterize this node for both euroc cams
 std::string frame = "";
@@ -51,6 +51,9 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr conveyor_cloud;
 
 void generate_conveyor_cloud(/*suturo_msgs::Task task*/)
 {
+  boost::posix_time::ptime s = boost::posix_time::microsec_clock::local_time();
+	
+	// task6_v1
 /* YAML info
   conveyor_belt:
     move_direction_and_length: [ 0, -2, 0 ]
@@ -65,46 +68,44 @@ void generate_conveyor_cloud(/*suturo_msgs::Task task*/)
 	v_dp.x = 0.3;
 	v_dp.y = 1.0;
 	v_dp.z = 0.2;
-	
+
 	pcl::PointXYZRGB v_mdl;
 	v_mdl.x = 0.0;
 	v_mdl.y = -2.0;
 	v_mdl.z = 0.0;
-	
+
 	double w = 0.1 * 2;
-	
+
 	conveyor_cloud = pcl::PointCloud<pcl::PointXYZRGB>::Ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
 	conveyor_cloud->height = 1;
 	conveyor_cloud->width = cloud_point_cnt;
 	conveyor_cloud->is_dense = false;
 	conveyor_cloud->points.resize(cloud_point_cnt);
-	
+
 	for (int i = 0; i < cloud_point_cnt; i++)
 	{
-		conveyor_cloud->points[i].x = v_dp.x + v_mdl.x * ((rand() % 1000) / 1000.0);
-		conveyor_cloud->points[i].y = v_dp.y + v_mdl.y * ((rand() % 1000) / 1000.0) + w * ((rand() % 1000) / 1000.0);
+		conveyor_cloud->points[i].x = v_dp.x + v_mdl.x * ((rand() % 1000) / 1000.0) + w * ((rand() % 1000) / 1000.0);
+		conveyor_cloud->points[i].y = v_dp.y + v_mdl.y * ((rand() % 1000) / 1000.0);
 		conveyor_cloud->points[i].z = v_dp.z + v_mdl.z * ((rand() % 1000) / 1000.0);
+	}
+
+  boost::posix_time::ptime e = boost::posix_time::microsec_clock::local_time();
+  if (verbose)
+	{
+		std::stringstream ss;
+		ss << "generated conveyor pointcloud for " << output_topic;
+		logger.logTime(s, e, ss.str());
 	}
 }
 
 
 void publish_conveyor_cloud(const ros::NodeHandle &nodeHandle)
 {
-  boost::posix_time::ptime s = boost::posix_time::microsec_clock::local_time();
-	
   sensor_msgs::PointCloud2 pub_message;
   pcl::toROSMsg(*conveyor_cloud, pub_message );
   pub_message.header.frame_id = frame;
   pub_message.header.stamp = ros::Time(0);
   pub_cloud.publish(pub_message);
-  
-  boost::posix_time::ptime e = boost::posix_time::microsec_clock::local_time();
-  if (verbose)
-	{
-		std::stringstream ss;
-		ss << "publishing conveyor pointcloud on " << output_topic;
-		logger.logTime(s, e, ss.str());
-	}
 }
 
 
@@ -119,6 +120,12 @@ int main (int argc, char** argv)
   pub_cloud = n.advertise<sensor_msgs::PointCloud2> (output_topic, 1);
 
 	generate_conveyor_cloud();
+	
+  if (verbose)
+	{
+		pcl::PCDWriter writer;
+		writer.write("/tmp/conveyor_cloud_.pcd", *conveyor_cloud);
+	}
 
 	ros::Rate loop_rate(10);
 	while (ros::ok())
