4e39d109c1bda99da123ab30f651528d20985c30
pmania <pmania@battlestation.local>
2014-10-30 18:07:13 +0100

    fallback mode for InitialAlignment in MPE implemented

--- suturo_perception_cad_recognition/src/model_pose_estimation.cpp
+++ suturo_perception_cad_recognition/src/model_pose_estimation.cpp
@@ -199,9 +199,6 @@ void ModelPoseEstimation::execute()
       continue;
     }
 
-    // Do the Pose estimation
-    // ICPFitter fitter(input_cloud, generated_models_->at(i), surface_normal_);
-
     // Workaround since the Point Types mismatch. We could win a ms here if we can avoid that
     pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_xyz (new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr model_cloud_xyz (new pcl::PointCloud<pcl::PointXYZ>);
@@ -214,55 +211,37 @@ void ModelPoseEstimation::execute()
     fitter.setIAMethod(icp_fitter_ia_method_);
 
     fitAndEvaluate(fitter,i);
-    // boost::posix_time::ptime s_icp = boost::posix_time::microsec_clock::local_time();
-    // fitter.execute();
-    // boost::posix_time::ptime e_icp = boost::posix_time::microsec_clock::local_time();
-    // logger_.logTime(s_icp, e_icp, "Time for ICPFitter::execute()");
-
-    // if(fitter.getFitnessScore() < fitness_score_)
-    // {
-    //   fitness_score_ = fitter.getFitnessScore();
-    //   // Get the orientation of the aligned object.
-    //   Eigen::Quaternionf orientation = fitter.getOrientation(); 
-    //   // Get the origin of the aligned object.
-    //   pcl::PointXYZ origin = fitter.getOrigin(); 
-    //   // logger_.logInfo("pipeline_mode_ = true");
-    //   suturo_msgs::Object &o = objects_->at(i);
-    //   ss << "Model " << i << "("<< o.name <<") is below best fitness score. "; 
-    //   ss << "Pose: " << orientation.x() << " " << orientation.y() << " " << orientation.z() << " " << orientation.w() << " " << origin << ". Score: " << fitter.getFitnessScore() << std::endl;
-    //   logger_.logInfo(ss.str());
-    //   ss.str("");
-
-    //   estimated_pose_ = Eigen::VectorXf(7);
-    //   estimated_pose_[0] = origin.x;
-    //   estimated_pose_[1] = origin.y;
-    //   estimated_pose_[2] = origin.z;
-
-    //   estimated_pose_[3] = orientation.x();
-    //   estimated_pose_[4] = orientation.y();
-    //   estimated_pose_[5] = orientation.z();
-    //   estimated_pose_[6] = orientation.w();
-
-    //   best_fit_model_ = i;
-    // }
-    // 
-    // // Dump the pointclouds that ICPFitter generated during it's execution
-    // if(dump_icp_fitter_pointclouds_)
-    //   fitter.dumpPointClouds();
-
-    // Workaround for a strange error ... The ICPFitter behaves
-    // differently when you instantiate the standard ICP around it ...
-    // Be careful when you comment this in .....
-    // pcl::IterativeClosestPointNonLinear<pcl::PointXYZ, pcl::PointXYZ> icp;
-    //
-    //
-   
   }
 
-  // Use another IA method, if we fail and the mode is enabled
-  if(fallback_ia_ && !poseEstimationSuccessful())
+  // Use another IA method, if we fail and the mode is enabled and atleast one
+  // model of interest is given (otherwise, the process will decrease in speed
+  // dramatically
+  if(fallback_ia_ && modelsOfInteresetGiven() && !poseEstimationSuccessful())
   {
+    logger_.logInfo("Falling back to MINMAX Initial Alignment");
+    for (int i = 0; i < objects_->size(); i++) 
+    {
+      // Is the current index a "model of interest"?
+      if(models_of_interest_.size()!=0 &&
+          std::find(models_of_interest_.begin(), models_of_interest_.end(), i)==models_of_interest_.end())
+      {
+        logger_.logInfo("Skipping model on users demand");
+        continue;
+      }
 
+      // Workaround since the Point Types mismatch. We could win a ms here if we can avoid that
+      pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud_xyz (new pcl::PointCloud<pcl::PointXYZ>);
+      pcl::PointCloud<pcl::PointXYZ>::Ptr model_cloud_xyz (new pcl::PointCloud<pcl::PointXYZ>);
+      copyPointCloud(*input_cloud, *input_cloud_xyz);
+      copyPointCloud(*generated_models_->at(i), *model_cloud_xyz);
+      ICPFitter fitter(input_cloud_xyz, model_cloud_xyz, surface_normal_);
+      fitter.rotateModelUp(false);
+      fitter.setMaxICPIterations(max_icp_iterations_);
+      fitter.setCalculateModelCentroid(true);
+      fitter.setIAMethod(ICPFitter::IA_MINMAX);
+
+      fitAndEvaluate(fitter,i);
+    }
   }
   // Store the result
   if(pipeline_mode_)
--- suturo_perception_cad_recognition/tests/tests.cpp
+++ suturo_perception_cad_recognition/tests/tests.cpp
@@ -952,6 +952,112 @@ TEST(suturo_perception_mbpe, pose_estimation_handlebar_minmax)
   SUCCEED();
 }
 
+// Use the fallback feature of MPE to detect the handlebar in the second iteration
+TEST(suturo_perception_mbpe, pose_estimation_handlebar_task2_with_fallback)
+{
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr input_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  std::string package_path = ros::package::getPath("suturo_perception_mbpe");
+  // std::string modelpath  = "test_files/005box_4000pts.pcd";
+  std::string objectpath = "test_files/handlebar_task2.pcd";
+  std::string object_table_normal_path = "test_files/handlebar_task2.pcd_table_normal";
+
+  if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (package_path + "/" + objectpath, *input_cloud) == -1)
+  {
+    PCL_ERROR ("Couldn't read input file\n");
+    exit (-1);
+  }
+
+  Eigen::Vector4f table_normal = getTableNormalFromStringLine(object_table_normal_path,package_path);
+  std::cout << "Using table normal: " << table_normal << std::endl;
+
+  // Prepare the model that should be matched against the input cloud
+  boost::shared_ptr<std::vector<suturo_msgs::Object> > objects(new std::vector<suturo_msgs::Object>);
+  suturo_msgs::Object obj;
+  obj.name="blue_handle";
+  obj.color="0000ff";
+  obj.description="a blue compound of a cylinder with two cubes";
+  obj.surface_material = suturo_msgs::Object::ALUMINIUM;
+
+  shape_msgs::SolidPrimitive shape1, shape2, shape3;
+	geometry_msgs::Pose pose1, pose2, pose3;
+  shape1.type = shape1.BOX;
+  // 0.05 x 0.05 x 0.05
+  shape1.dimensions.push_back(0.05f);
+  shape1.dimensions.push_back(0.05f);
+  shape1.dimensions.push_back(0.05f);
+  pose1.position.x = 0;
+  pose1.position.y = 0;
+  pose1.position.z = 0;
+  pose1.orientation.x = 0;
+  pose1.orientation.y = 0;
+  pose1.orientation.z = 0;
+  pose1.orientation.w = 1;
+
+  // Define the extra parts on the handlebar
+  shape2.type = shape1.BOX;
+  // 0.05 x 0.05 x 0.05
+  shape2.dimensions.push_back(0.05f);
+  shape2.dimensions.push_back(0.05f);
+  shape2.dimensions.push_back(0.05f);
+  pose2.position.x = 0;
+  pose2.position.y = 0;
+  pose2.position.z = 0.35f;
+  pose2.orientation.x = 0;
+  pose2.orientation.y = 0;
+  pose2.orientation.z = 0;
+  pose2.orientation.w = 1;
+
+  shape3.type = shape1.CYLINDER;
+  shape3.dimensions.push_back(0.3f);
+  shape3.dimensions.push_back(0.01f);
+  pose3.position.x = 0;
+  pose3.position.y = 0;
+  pose3.position.z = 0.175f;
+  pose3.orientation.x = 0;
+  pose3.orientation.y = 0;
+  pose3.orientation.z = 0;
+  pose3.orientation.w = 1;
+
+
+  obj.primitives.push_back(shape1);
+  obj.primitives.push_back(shape2);
+  obj.primitives.push_back(shape3);
+  obj.primitive_poses.push_back(pose1);
+  obj.primitive_poses.push_back(pose2);
+  obj.primitive_poses.push_back(pose3);
+  objects->push_back(obj);
+
+  suturo_perception::PipelineData::Ptr data_;
+  suturo_perception::PipelineObject::Ptr object_;
+  ModelPoseEstimation mpe(objects,data_,object_);
+  mpe.setInputCloud(input_cloud);
+  mpe.setSurfaceNormal(table_normal);
+  // mpe.setInitialAlignmentMethod(ICPFitter::IA_MINMAX);
+  std::vector<int> modelsOfInterest;
+  modelsOfInterest.push_back(0); // only match against the cylinder (and fail)
+  mpe.setModelsOfInterest(modelsOfInterest);
+  mpe.setVoxelSize(0.003f);
+  mpe.setFallbackInitialAlignmentEnabled(true);
+  mpe.setDumpICPFitterPointclouds(true); // Enable debugging. This will save pointclouds
+  mpe.execute();
+
+  std::cout << "Fitness for handlebar matching: " << mpe.getFitnessScore() << std::endl;
+  // The estimation should be succesful
+	ASSERT_TRUE( mpe.poseEstimationSuccessful() );
+  // Check the pose (origin.x, origin.y, origin.z)
+  // ASSERT_NEAR( mpe.getEstimatedPose()[0],  -0.320974, CAD_RECOGNITION_TEST_ACCEPTABLE_POINT_ERROR );
+  // ASSERT_NEAR( mpe.getEstimatedPose()[1],  -0.100329, CAD_RECOGNITION_TEST_ACCEPTABLE_POINT_ERROR );
+  // ASSERT_NEAR( mpe.getEstimatedPose()[2],    1.02429, CAD_RECOGNITION_TEST_ACCEPTABLE_POINT_ERROR );
+
+  // // Check the orientation (x,y,z,w from a Quaternionf)
+  // ASSERT_NEAR( mpe.getEstimatedPose()[3],   0.876425, CAD_RECOGNITION_TEST_ACCEPTABLE_ORIENTATION_ERROR );
+  // ASSERT_NEAR( mpe.getEstimatedPose()[4], -0.0972819, CAD_RECOGNITION_TEST_ACCEPTABLE_ORIENTATION_ERROR );
+  // ASSERT_NEAR( mpe.getEstimatedPose()[5],    0.17143, CAD_RECOGNITION_TEST_ACCEPTABLE_ORIENTATION_ERROR );
+  // ASSERT_NEAR( mpe.getEstimatedPose()[6],  -0.439349, CAD_RECOGNITION_TEST_ACCEPTABLE_ORIENTATION_ERROR );
+
+  SUCCEED();
+}
+
 // TODO
 // Tests for
 // - no surface normal set
